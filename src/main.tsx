099a1b33ca7164ef81847e0d907dfa52d8be8b0a
Jonathan Cornwell <jonathan.cornwell@bsu.edu>
2025-04-05 15:38:58 -0700

    add chakra ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 // import { StrictMode } from "react";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import PixiApp from "./PixiApp.tsx";
 
 createRoot(document.getElementById("root")!).render(
   // removed <StrictMode> wrapper for now to avoid double rendering in dev
-
-  <PixiApp height={400} width={800} />
+  <ChakraProvider value={defaultSystem}>
+    <ThemeProvider attribute="class" disableTransitionOnChange>
+      <PixiApp height={400} width={800} />
+    </ThemeProvider>
+  </ChakraProvider>
 );
