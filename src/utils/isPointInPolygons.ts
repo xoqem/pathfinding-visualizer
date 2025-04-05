479740bb1b14f47c390b2ab3df25f4b0c437d3a0
Jonathan Cornwell <jonathan.cornwell@bsu.edu>
2025-04-05 15:38:58 -0700

    Cleaner point in polygon check

--- src/utils/isPointInPolygons.ts
+++ src/utils/isPointInPolygons.ts
@@ -1,25 +1,9 @@
-import { PointData, pointInTriangle } from "pixi.js";
+import { PointData, Polygon } from "pixi.js";
 
 function isPointInPolygon(point: PointData, polygon: PointData[] | null) {
   if (!polygon) return false;
 
-  for (let i = 1; i < polygon.length - 1; i++) {
-    if (
-      pointInTriangle(
-        point.x,
-        point.y,
-        polygon[0].x,
-        polygon[0].y,
-        polygon[i].x,
-        polygon[i].y,
-        polygon[i + 1].x,
-        polygon[i + 1].y
-      )
-    ) {
-      return true;
-    }
-  }
-  return false;
+  return new Polygon(polygon).contains(point.x, point.y);
 }
 
 export default function isPointInPolygons(
