a32f1765581d77ee4dc3640543d3c6dae6d7a6bb
Jonathan Cornwell <jonathan.cornwell@bsu.edu>
2025-04-05 15:38:58 -0700

    Fetch svg and display and console log as string

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Application, extend } from "@pixi/react";
 import { Container, Graphics } from "pixi.js";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 
 import "./App.css";
 
@@ -9,7 +9,18 @@ extend({
   Graphics,
 });
 
+async function fetchSvgAsString(svgPath: string){
+  const response = await fetch(svgPath);
+  const text = await response.text();
+  console.log(text);
+}
+
+
 export default function App() {
+  useEffect(() => {
+    fetchSvgAsString('./public/example.svg');
+  }, []);
+
   const drawCallback = useCallback((graphics: Graphics) => {
     graphics.clear();
     graphics.setFillStyle({ color: "red" });
